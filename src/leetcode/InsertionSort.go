ae675f7c17dde50ffd7cf58daee4996dfe332c5b
renfakai <renfakai@58.com>
2021-09-06 16:04:28 +0800

    add insertSort

--- src/leetcode/InsertionSort.go
+++ src/leetcode/InsertionSort.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // 直接插入排序（Straight Insertion Sort）是一种最简单的排序方法，其基本操作是将一条记录插入到已排好序的有序表中，从而得到一个新的、记录数量增1的有序表。
 func insertSort(num []int) []int {
 	// 总长度
@@ -13,4 +15,34 @@ func insertSort(num []int) []int {
 		}
 	}
 	return num
+}
+
+//
+func insertSortWithFront(num []int) []int {
+	length := len(num)
+	// 不能从0开始因为第一个元素要跟0对比
+	for i := 0; i < length; i++ {
+		successIndex := i
+		insertV := num[i]
+		for j := 0; j < i; j++ {
+			if num[j] > num[i] {
+				successIndex = j
+				break
+			}
+		}
+		for j := i; j > successIndex; j-- {
+			num[j] = num[j-1]
+		}
+		num[successIndex] = insertV
+	}
+	return num
+}
+
+func main() {
+	intItems := make([]int, 10)
+	for i := 9; i >= 0; i-- {
+		intItems[9-i] = i
+	}
+	insertSortWithFront(intItems)
+	fmt.Println(intItems)
 }
