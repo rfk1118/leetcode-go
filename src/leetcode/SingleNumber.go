5bcbf80a2eaf079fcd6d94b8671f5c2013402cad
renfakai <renfakai@58.com>
2021-09-06 17:17:21 +0800

    findTwoSingleNumber

--- src/leetcode/SingleNumber.go
+++ src/leetcode/SingleNumber.go
@@ -1,9 +1,35 @@
 package main
 
+import "fmt"
+
 func singleNumber(nums []int) int {
-	error := 0
+	er := 0
 	for _, num := range nums {
-		error = error ^ num
+		er = er ^ num
 	}
-	return error
+	return er
+}
+
+// ^ 如果相对应位值相同，则结果为0，否则为1
+// ~ 按位取反运算符翻转操作数的每一位，即0变成1，1变成0。
+func findTwoSingleNumber(nums []int) {
+	// 先找到a^b
+	er := 0
+	other := 0
+	for _, num := range nums {
+		er = er ^ num
+	}
+	// 找到第二个数
+	rightOne := er & (^er + 1)
+	for _, num := range nums {
+		if (num & rightOne) != 0 {
+			other = other ^ num
+		}
+	}
+	fmt.Printf("other:%d,another:%d", other, er^other)
+}
+
+func main() {
+	nums := []int{1, 1, 1, 2, 2, 3, 3, 5}
+	findTwoSingleNumber(nums)
 }
